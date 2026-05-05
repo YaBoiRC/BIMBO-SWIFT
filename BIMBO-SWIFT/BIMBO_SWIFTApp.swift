076473c511dfdcaab6bd49640efb6770e8aea4fe
Raymond Chavez <yaboircyt@gmail.com>
2026-05-04 20:21:28 -0600

    Initial Commit

---
+++ BIMBO-SWIFT/BIMBO_SWIFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIMBO_SWIFTApp.swift
+//  BIMBO-SWIFT
+//
+//  Created by Raymond Chavez on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BIMBO_SWIFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
