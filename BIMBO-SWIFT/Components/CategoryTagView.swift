57827226657e2406dbf49ea49b6ae31befd03a45
Rojoosam <vandier12@outlook.com>
2026-05-04 20:40:04 -0600

    Initial commit

---
+++ BIMBO-SWIFT/Components/CategoryTagView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct CategoryTagView: View {
+    let title: String
+
+    var body: some View {
+        Text(title)
+            .font(.caption.weight(.semibold))
+            .foregroundStyle(AppColors.primaryBlue)
+            .padding(.horizontal, 12)
+            .padding(.vertical, 8)
+            .background(
+                Capsule()
+                    .fill(AppColors.primaryBlue.opacity(0.12))
+            )
+    }
+}
